7b440b8d4a18c1abd0757bd2a2b7a841d68e5c28
xBarikadex <71157911+xBarikadex@users.noreply.github.com>
2021-04-22 01:39:45 -0700

    Homepage and Resume 90%

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,29 @@
 function replace(page) {
     window.location=(page + ".html");
 }
+
+function buttonsa() {
+    console.log("Hallo");
+    document.getElementById("buttons").style.color = "red";
+}
+
+$("#linkedIn").on("mouseenter", function() {
+    $(this).attr("src", "images/linkinHover.png");
+})
+$("#linkedIn").on("mouseleave", function() {
+    $(this).attr("src", "images/linkedin.png");
+})
+
+$("#steam").on("mouseenter", function() {
+    $(this).attr("src", "images/steamHover.png");
+})
+$("#steam").on("mouseleave", function() {
+    $(this).attr("src", "images/steam.png");
+})
+
+$("#discord").on("mouseenter", function() {
+    $(this).attr("src", "images/discordHover.png");
+})
+$("#discord").on("mouseleave", function() {
+    $(this).attr("src", "images/discord.png");
+})
